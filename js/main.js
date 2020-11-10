7e57f405a8839e4edada471c8183ec7e5625b6c7
Phillip Lam <plam89@ucla.edu>
2020-11-10 11:39:08 -0800

    slideshow working properly

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 var xhr = new XMLHttpRequest();
 var $h2 = document.querySelector('h2')
+var interval = null
+var count = 0
+
+
 xhr.open('GET', 'https://www.thecocktaildb.com/api/json/v2/9973533/popular.php');
 xhr.responseType = 'json';
 xhr.addEventListener('load', function () {
@@ -7,10 +11,22 @@ xhr.addEventListener('load', function () {
   console.log(xhr.response);
   data = xhr.response
 
-  var img = document.createElement('img')
-  img.setAttribute('src', data.drinks[0].strDrinkThumb)
-  img.className = 'slideshow-img'
-  $h2.appendChild(img)
+  interval = setInterval(function(){
+    var img = document.createElement('img')
+    img.setAttribute('src', data.drinks[count].strDrinkThumb)
+    count++
+    img.className = 'slideshow-img'
+    $h2.appendChild(img)
+    setTimeout(function(){
+      $h2.removeChild(img)
+    },2000)
+
+    if (count === 11) {
+      count = 0
+    }
+
+  },2000)
+
 });
 
 xhr.send();
