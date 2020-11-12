a113ae49e7ceee90810391d202cc0d7a2050d829
Phillip Lam <plam89@ucla.edu>
2020-11-11 16:23:22 -0800

    Finished feature 2, refactored slideshow code, added random-button functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 var xhr = new XMLHttpRequest();
-var $h2 = document.querySelector('h2');
 var interval = null;
 var count = 1;
 var $cocktails = document.querySelector('.cocktails-text');
 var data2 = null;
+var $firstImg = document.querySelector('.first');
+
 xhr.open('GET', 'https://www.thecocktaildb.com/api/json/v2/9973533/popular.php');
 xhr.responseType = 'json';
 
@@ -14,49 +15,54 @@ xhr.addEventListener('load', function () {
     if (count === xhr.response.drinks.length) {
       count = 0;
     }
-    document.querySelector('.first').classList.add('hidden');
-    var img = document.createElement('img');
-    img.setAttribute('src', data.drinks[count].strDrinkThumb);
+    $firstImg.setAttribute('src', data.drinks[count].strDrinkThumb);
     $cocktails.textContent = data.drinks[count].strDrink;
-    img.className = 'slideshow-img';
+    $firstImg.className = 'slideshow-img';
+
     count++;
-
-    $h2.appendChild(img);
-
-    setTimeout(function () {
-      $h2.removeChild(img);
-    }, 1000);
-
-  }, 1000);
+  }, 2000);
 });
-xhr.send();
 
+xhr.send();
 document.addEventListener('click', function (event) {
   if (event.target.className === 'fas fa-chevron-right right-arrow') {
-    count++;
-    $cocktails.textContent = '';
     clearInterval(interval);
-
-    var img = document.createElement('img');
-    img.setAttribute('src', data2.drinks[count].strDrinkThumb);
+    count++;
+    if (count === xhr.response.drinks.length) {
+      count = 0;
+    }
+    $firstImg.setAttribute('src', data2.drinks[count].strDrinkThumb);
     $cocktails.textContent = data2.drinks[count].strDrink;
 
-    count++;
     interval = setInterval(function () {
       if (count === xhr.response.drinks.length) {
         count = 0;
       }
-      document.querySelector('.first').classList.add('hidden');
-      var img = document.createElement('img');
-      img.setAttribute('src', data2.drinks[count].strDrinkThumb);
+      $firstImg.setAttribute('src', data2.drinks[count].strDrinkThumb);
       $cocktails.textContent = data2.drinks[count].strDrink;
-      img.className = 'slideshow-img';
-      count++;
-      $h2.appendChild(img);
-      setTimeout(function () {
-        $h2.removeChild(img);
-      }, 2500);
 
-    }, 2500);
+      count++;
+    }, 2000);
+  } else if (event.target.className === 'fas fa-chevron-left left-arrow') {
+    clearInterval(interval);
+    count--;
+    if (count < 0) {
+      count = xhr.response.drinks.length - 1;
+    }
+    $firstImg.setAttribute('src', data2.drinks[count].strDrinkThumb);
+    $cocktails.textContent = data2.drinks[count].strDrink;
+
+    interval = setInterval(function () {
+      if (count === xhr.response.drinks.length) {
+        count = 0;
+      }
+      $firstImg.setAttribute('src', data2.drinks[count].strDrinkThumb);
+      $cocktails.textContent = data2.drinks[count].strDrink;
+      count++;
+    }, 2000);
+  } else if (event.target.className === 'random-button') {
+    document.querySelector('#home-page').classList.add('hidden');
+    document.querySelector('#random-page').classList.remove('hidden');
   }
+
 });
