e00ffa8a905df6427fabc1f102f38948b9d6692c
Phillip Lam <plam89@ucla.edu>
2020-11-11 22:33:35 -0800

    added ABC list correctly

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var count = 1;
 var $cocktails = document.querySelector('.cocktails-text');
 var data2 = null;
 var $firstImg = document.querySelector('.first');
-
+// var drinkList = null
 xhr.open('GET', 'https://www.thecocktaildb.com/api/json/v2/9973533/popular.php');
 xhr.responseType = 'json';
 
@@ -64,11 +64,30 @@ document.addEventListener('click', function (event) {
     document.querySelector('#home-page').classList.add('hidden');
     document.querySelector('#random-page').classList.remove('hidden');
     document.querySelector('#browse-page').classList.add('hidden');
+    document.querySelector('.browse').classList.add('hidden');
   } else if (event.target.className === 'icons browse' || event.target.id === 'browse-icon') {
     document.querySelector('#home-page').classList.add('hidden');
     document.querySelector('#random-page').classList.add('hidden');
     document.querySelector('#browse-page').classList.remove('hidden');
+    document.querySelector('.browse').classList.add('hidden');
+  } else if (event.target.className === 'letters') {
+    var $span = document.querySelectorAll('span');
+    for (var i = 0; i < $span.length; i++) {
+      $span[i].style = 'border: none'; // this allows the letter clicked to be underlined. Will clear all underlines first.
+    }
+    event.target.style = 'border-bottom: 3px solid black';
 
+    var xhr2 = new XMLHttpRequest();
+    xhr2.open('GET', 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=' + event.target.textContent);
+    xhr2.responseType = 'json';
+    xhr2.addEventListener('load', function () {
+      // drinkList = xhr2.response
+      document.querySelector('.cocktails-letter').textContent = event.target.textContent;
+      document.querySelector('.cocktails-letter').setAttribute('class', 'cocktails-letter');
+      document.querySelector('.cocktails-letter').classList.add('sticky');
+    });
+
+    xhr2.send();
   }
 
 });
