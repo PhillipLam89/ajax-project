8369f529b6e22598eee8e3e34cb29ce4498b41f9
Phillip Lam <plam89@ucla.edu>
2020-11-12 00:46:44 -0800

    started on random

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var count = 1;
 var $cocktails = document.querySelector('.cocktails-text');
 var data2 = null;
 var $firstImg = document.querySelector('.first');
-// var drinkList = null
+var drinkList = null;
 xhr.open('GET', 'https://www.thecocktaildb.com/api/json/v2/9973533/popular.php');
 xhr.responseType = 'json';
 
@@ -22,6 +22,8 @@ xhr.addEventListener('load', function () {
     count++;
   }, 2000);
 });
+
+var $recipeDiv = document.querySelector('#recipe-img-div');
 
 xhr.send();
 document.addEventListener('click', function (event) {
@@ -70,10 +72,12 @@ document.addEventListener('click', function (event) {
     document.querySelector('#random-page').classList.add('hidden');
     document.querySelector('#browse-page').classList.remove('hidden');
     document.querySelector('.browse').classList.add('hidden');
+    $recipeDiv.innerHTML = '';
   } else if (event.target.className === 'letters') {
     var $span = document.querySelectorAll('span');
     for (var i = 0; i < $span.length; i++) {
       $span[i].style = 'border: none'; // this allows the letter clicked to be underlined. Will clear all underlines first.
+
     }
     event.target.style = 'border-bottom: 3px solid black';
 
@@ -81,13 +85,63 @@ document.addEventListener('click', function (event) {
     xhr2.open('GET', 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=' + event.target.textContent);
     xhr2.responseType = 'json';
     xhr2.addEventListener('load', function () {
-      // drinkList = xhr2.response
+      drinkList = xhr2.response;
+
       document.querySelector('.cocktails-letter').textContent = event.target.textContent;
       document.querySelector('.cocktails-letter').setAttribute('class', 'cocktails-letter');
       document.querySelector('.cocktails-letter').classList.add('sticky');
+
+      var $recipeList = document.querySelector('#recipe-list');
+      var $ol = document.createElement('ol');
+      $recipeList.innerHTML = '';
+      $recipeList.appendChild($ol);
+
+      if (drinkList.drinks === null) {
+        drinkList.drinks = [''];
+        var $li = document.createElement('li');
+        $li.setAttribute('class', 'no-recipes');
+        $li.textContent = 'SORRY but there are no recipes beginning with ' + event.target.textContent;
+        document.querySelector('.sticky').classList.add('hidden');
+        $ol.appendChild($li);
+      }
+
+      for (var j = 0; j < drinkList.drinks.length; j++) {
+        var $li2 = document.createElement('li');
+        $li.textContent = drinkList.drinks[j].strDrink;
+        $li.setAttribute('class', 'recipe-link');
+        $ol.appendChild($li2);
+      }
     });
 
     xhr2.send();
-  }
+  } else if (event.target.className === 'recipe-link') {
+    document.querySelector('#home-page').classList.add('hidden');
+    document.querySelector('#random-page').classList.add('hidden');
+    document.querySelector('#browse-page').classList.add('hidden');
+    document.querySelector('.browse').classList.remove('hidden');
+    document.querySelector('#recipe-page').classList.remove('hidden');
+    document.querySelector('#top-header').textContent = event.target.textContent;
 
+    var $img = document.createElement('img');
+
+    for (var k = 0; k < drinkList.drinks.length; k++) {
+      if (drinkList.drinks[k].strDrink === event.target.textContent) {
+        $img.setAttribute('src', drinkList.drinks[k].strDrinkThumb);
+        $img.className = 'slideshow-img';
+        $recipeDiv.appendChild($img);
+      }
+    }
+  }
 });
+
+// var xhr3 = new XMLHttpRequest();
+
+// xhr3.open('GET', 'https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?c=Cocktail');
+// xhr3.responseType = 'json';
+
+// xhr3.addEventListener('load', function () {
+
+//   var randomData = xhr3.response;
+
+// })
+// xhr3.send();
